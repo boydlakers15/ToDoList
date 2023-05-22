3ac4e36d1b19d4b73b32d11a6747e0c0cfe1acdc
Jared Boyd <jboyd115@icloud.com>
2023-05-22 14:36:34 +0200

    Making changes to the todo list

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -4,8 +4,27 @@ import SignUpPage from './comp/SignUpPage';
 import React from 'react';
 import LoginPage from './comp/LoginPage';
 import TodoList from './comp/TodoList';
+import axios from 'axios';
+import { useState, useEffect } from "react";
 
 function App() {
+  const [todos, setTodos] = useState([])
+  useEffect(() => {
+    // Fetch todos from the server
+    fetchTodos();
+  }, []);
+  
+  const fetchTodos = async () => {
+      try {
+      const response = await axios.get("http://localhost:5001/api/alltodos");
+      console.log("db:", response.data)
+      setTodos(response.data);
+    } catch (error) {
+        console.error("Error fetching todos:", error);
+    }
+};
+
+console.log("todos:", todos)
   
   return (
     <Router>
